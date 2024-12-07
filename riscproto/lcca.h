7b9823f3aa5d1c8251a2698417817dc75470f3d2
vmlinuz719 <vmlinuz719@gmail.com>
2024-12-07 18:51:12 -0500

    prevent macro explosion

--- riscproto/lcca.h
+++ riscproto/lcca.h
@@ -6,15 +6,41 @@
 
 #include "error.h"
 
+#define EXT4(x)             ((x) & (1L << 3)  ? (x) | 0xFFFFFFFFFFFFFFF0 : (x))
+#define EXT8(x)             ((x) & (1L << 7)  ? (x) | 0xFFFFFFFFFFFFFF00 : (x))
+#define EXT10(x)            ((x) & (1L << 9)  ? (x) | 0xFFFFFFFFFFFFFC00 : (x))
+#define EXT12(x)            ((x) & (1L << 11) ? (x) | 0xFFFFFFFFFFFFF000 : (x))
+#define EXT16(x)            ((x) & (1L << 15) ? (x) | 0xFFFFFFFFFFFF0000 : (x))
+#define EXT20(x)            ((x) & (1L << 19) ? (x) | 0xFFFFFFFFFFF00000 : (x))
+#define EXT32(x)            ((x) & (1L << 31) ? (x) | 0xFFFFFFFF00000000 : (x))
+#define EXT48(x)            ((x) & (1L << 47) ? (x) | 0xFFFF000000000000 : (x))
+
+#define OPCODE(x)           ((x) >> 28)
+#define RA(x)               (((x) >> 23) & 0x1F)
+#define RB(x)               (((x) >> 5) & 0x1F)
+#define RC(x)               ((x) & 0x1F)
+#define FN(x)               (((x) >> 20) & 0x7)
+
+#define RR_IMM(x)           (((x) >> 10) & 0x3FF)
+
 typedef struct lcca_t {
     lcca_bus_t *bus;
     pthread_t *run;
 
-    uint64_t regs[64];
+    uint64_t regs[32];
+    uint64_t c_regs[32];
     uint64_t pc;
 
     void (*operations[16]) (struct lcca_t *, uint32_t);
 } lcca_t;
+
+uint64_t get_reg_l(lcca_t *, int);
+void set_reg_l(lcca_t *, int, uint64_t);
+
+uint64_t get_reg_q(lcca_t *, int);
+void set_reg_q(lcca_t *, int, uint64_t);
+
+void lcca32_rr_0(lcca_t *, uint32_t);
 
 #endif
 
--- riscproto/riscproto.c
+++ riscproto/riscproto.c
@@ -7,3 +7,82 @@
 #include "error.h"
 #include "mmio.h"
 #include "lcca.h"
+
+uint64_t get_reg_l(lcca_t *cpu, int reg) {
+    if (reg == 0) {
+        return 0;
+    }
+
+    else {
+        uint64_t result = cpu->regs[reg] & 0xFFFFFFFF;
+        return EXT32(result);
+    }
+}
+
+void set_reg_l(lcca_t *cpu, int reg, uint64_t value) {
+    if (reg != 0) {
+        uint64_t result = value & 0xFFFFFFFF;
+        cpu->regs[reg] = EXT32(result);
+    }
+}
+
+uint64_t get_reg_q(lcca_t *cpu, int reg) {
+    if (reg == 0) {
+        return 0;
+    }
+
+    else {
+        return cpu->regs[reg];
+    }
+}
+
+void set_reg_q(lcca_t *cpu, int reg, uint64_t value) {
+    if (reg != 0) {
+        cpu->regs[reg] = value;
+    }
+}
+
+uint64_t shl(uint64_t x, uint64_t shamt) {
+    if (shamt == 64) return 0;
+    else if (shamt < 64) return x << shamt;
+    else return x;
+}
+
+uint64_t shr(uint64_t x, uint64_t shamt) {
+    if (shamt == 64) return 0;
+    else if (shamt < 64) return x >> shamt;
+    else return x;
+}
+
+uint64_t sar(uint64_t x, uint64_t shamt) {
+    if (shamt == 64) return 0;
+    else if (shamt < 64) return (uint64_t) (((int64_t) x) >> shamt);
+    else return x;
+}
+
+uint64_t sh(uint64_t x, int shamt) {
+    if (shamt < 0) return shr(x, -shamt);
+    else return shl(x, shamt);
+}
+
+uint64_t sha(uint64_t x, int shamt) {
+    if (shamt < 0) return sar(x, -shamt);
+    else return shl(x, shamt);
+}
+
+void lcca32_rr_0(lcca_t *cpu, uint32_t inst) {
+    uint64_t b = get_reg_l(cpu, RB(inst));
+    uint64_t c = get_reg_l(cpu, RC(inst));
+    uint64_t d = RR_IMM(inst);
+    d = EXT10(d);
+
+    switch (FN(inst)) {
+        case 0: set_reg_l(cpu, RA(inst), b + c + d); break;
+        case 1: set_reg_l(cpu, RA(inst), b - (c + d)); break;
+        case 2: set_reg_l(cpu, RA(inst), b & (c | d)); break;
+        case 3: set_reg_l(cpu, RA(inst), b | (c & ~d)); break;
+        case 4: set_reg_l(cpu, RA(inst), b ^ (c ^ d)); break;
+        case 5: set_reg_l(cpu, RA(inst), sh(b, c + d)); break;
+        case 6: set_reg_l(cpu, RA(inst), sha(b, c + d)); break;
+    }
+}
