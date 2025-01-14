be9e7443cf8a8b5661a162d42b36f5f5948566c2
vmlinuz719 <vmlinuz719@gmail.com>
2025-01-14 17:20:26 -0500

    pineapple sucks

--- riscproto/tlb_test/tlb.c
+++ riscproto/tlb_test/tlb.c
@@ -7,22 +7,20 @@
 
 #define TLB_SIZE 128
 
-struct cam_index {
+#define PGID(x) ((x) & 0x3FF) // 10-bit Page Group Identifiers
+
+struct tlb_entry {
     uint64_t key;
-    int index;
+    uint64_t value;
     UT_hash_handle hh;
 };
 
-struct cam_entry {
-    uint64_t key;
-    uint64_t value;
-    int valid;
+struct tlb {
+    struct tlb_entry entries[TLB_SIZE];
+    struct tlb_entry *hash;
 };
 
-struct cam {
-    struct cam_entry entries[TLB_SIZE];
-    struct cam_index *table;
-};
+
 
 int main(int argc, char *argv[]) {
     return 0;
