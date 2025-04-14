ef380d76413f27b3449171d9520325eb0bc51478
vmlinuz719 <vmlinuz719@gmail.com>
2025-04-14 14:18:59 -0400

    port write

--- riscproto/slim_test/port.c
+++ riscproto/slim_test/port.c
@@ -4,3 +4,12 @@
 #include <string.h>
 #include <pthread.h>
 
+typedef struct port {
+    void *ctx;
+    void (*port_write) (void *, uint8_t);
+} slim_port_t;
+
+void port_write(slim_port_t *port, uint8_t data) {
+    port->port_write(port->ctx, data);
+}
+
