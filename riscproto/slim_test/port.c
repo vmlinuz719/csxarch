94f35c4401f8568dc9a4d3bb815ca5a68e67b847
vmlinuz719 <vmlinuz719@gmail.com>
2025-04-17 17:23:06 -0400

    routing implemented

--- riscproto/slim_test/port.c
+++ riscproto/slim_test/port.c
@@ -58,7 +58,7 @@ void router_rx_write(void *vctx, uint8_t ch) {
         uint16_t header = ((uint16_t) (ctx->buf[0])) << 8;
         header |= (uint16_t) (ctx->buf[1]);
 
-        uint16_t size = header & 0x8FF;
+        uint16_t size = header & 0x7FF;
         if (size == 0) {
             ctx->size = 8;
         } else if (size == 0x8FF) {
@@ -88,6 +88,57 @@ void router_rx_write(void *vctx, uint8_t ch) {
     }
 }
 
+typedef struct hexdump_port_ctx {
+    pthread_mutex_t *hexdump_lock;
+    uint8_t buf[RX_BUF_SIZE];
+    uint16_t index;
+    int id, size;
+} hexdump_ctx_t;
+
+void hexdump_write(void *vctx, uint8_t ch) {
+    hexdump_ctx_t *ctx = (hexdump_ctx_t *) vctx;
+
+    ctx->buf[ctx->index++] = ch;
+    if (!(ctx->size) && ctx->index == 2) {
+        uint16_t header = ((uint16_t) (ctx->buf[0])) << 8;
+        header |= (uint16_t) (ctx->buf[1]);
+
+        uint16_t size = header & 0x7FF;
+        if (size == 0) {
+            ctx->size = 8;
+        } else if (size == 0x8FF) {
+            ctx->index = 0;
+            ctx->size = 0;
+        } else {
+            ctx->size = size;
+        }
+    } else if (ctx->size && ctx->index >= ctx->size) {
+        pthread_mutex_lock(ctx->hexdump_lock);
+
+        printf("%d:\n", ctx->id);
+
+        for (int i = 0; i < ctx->size; i++) {
+            printf("%02hhX ", ctx->buf[i]);
+            if (i % 16 == 15 || i + 1 == ctx->size) {
+                printf("\n");
+            }
+        }
+
+        pthread_mutex_unlock(ctx->hexdump_lock);
+
+        ctx->index = 0;
+        ctx->size = 0;
+    }
+}
+
+void hexdump_port_init(hexdump_ctx_t *ctx) {
+    pthread_mutex_init(ctx->hexdump_lock, NULL);
+}
+
+void hexdump_port_destroy(hexdump_ctx_t *ctx) {
+    pthread_mutex_destroy(ctx->hexdump_lock);
+}
+
 void router_init(slim_router_t *router) {
     for (int i = 0; i < ROUTER_PORTS; i++) {
         router->rx[i].ctx = &(router->rx_ctx[i]);
@@ -108,4 +159,30 @@ void router_destroy(slim_router_t *router) {
     for (int i = 0; i < ROUTER_PORTS; i++) {
         pthread_mutex_destroy(&(router->tx.tx_lock[i]));
     }
+}
+
+int main(int argc, char *argv[]) {
+    slim_router_t router;
+    router_init(&router);
+    
+    pthread_mutex_t hexdump_mutex;
+    hexdump_ctx_t hex_port;
+    hex_port.hexdump_lock = &hexdump_mutex;
+    hexdump_port_init(&hex_port);
+
+    router.tx.p[2].ctx = &hex_port;
+    router.tx.p[2].port_write = hexdump_write;
+
+    port_write(&(router.rx[5]), 0x28);
+    port_write(&(router.rx[5]), 0x00);
+    port_write(&(router.rx[5]), 0x00);
+    port_write(&(router.rx[5]), 0x00);
+    port_write(&(router.rx[5]), 0xDE);
+    port_write(&(router.rx[5]), 0xAD);
+    port_write(&(router.rx[5]), 0xBE);
+    port_write(&(router.rx[5]), 0xEF);
+
+    hexdump_port_destroy(&hex_port);
+    router_destroy(&router);
+    return 0;
 }
